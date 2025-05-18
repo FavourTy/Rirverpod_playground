845b6d471a74974d9bd9c6bad9368f8177713416
favourTy <favouradetayo03@gmail.com>
2025-05-18 17:56:39 +0100

    added consumer widget

--- weather_app/lib/homepage.dart
+++ weather_app/lib/homepage.dart
@@ -1,10 +1,23 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class Homepage extends StatelessWidget {
-  const Homepage({super.key});
-
+class Homepage extends ConsumerWidget {
+  const Homepage({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Weather App"),
+      ),
+      body: Center(
+        child: Text(
+          "",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
   }
 }
